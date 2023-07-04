3def10c4540e1a5a9d7076bb6d1bccbdd32c6835
Abizard Shafwan <abizard@pop-os.localdomain>
2023-07-04 13:17:53 +0700

    Update: remove bugsnag from index

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
 require('@/bootstrap');
 import { appConfig } from 'Config/app';
-import Bugsnag from '@bugsnag/js';
-import BugsnagPluginAwsLambda from '@bugsnag/plugin-aws-lambda';
-
-// Prepare bugsnag
-if (appConfig.bugsnagKey) {
-  Bugsnag.start({
-    releaseStage: appConfig.environment,
-    enabledReleaseStages: ['production', 'beta'],
-    apiKey: appConfig.bugsnagKey,
-    plugins: [BugsnagPluginAwsLambda],
-  });
-}
 
 // Init App Server
 let appServer: any;
 if (appConfig.server === 'lambda') {
-  const { server } = require('Server/lambda');
+  const { server } =  require('Server/lambda');
   appServer = server;
-  const bugsnagHandler = Bugsnag?.getPlugin('awsLambda')?.createHandler();
-  exports.handler = bugsnagHandler ? bugsnagHandler(appServer) : appServer;
+  exports.handler = server;
 } else {
   appServer = require('Server/http-server');
 }
